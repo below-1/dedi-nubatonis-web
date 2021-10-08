023979905f422d186e559d0e82bc62d1ea48ea77
jordan meta <jordanmeta@mail.com>
2021-10-08 22:46:54 +0800

    savepoint

--- src/components/QuickFooter.vue
+++ src/components/QuickFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="flex items-center justify-center h-12 border-t border-gray-200">
-    <h3 class="text-gray-500 font-medium">Copyright Dedi Nubatonis, 2021</h3>
+  <footer class="flex items-center justify-center h-12">
+    <h3 class="font-medium">Copyright Dedi Nubatonis, 2021</h3>
   </footer>
 </template>
--- src/components/landing/Landing.vue
+++ src/components/landing/Landing.vue
@@ -1,16 +1,83 @@
 <script setup>
-import QuickFooter from '@quick/components/QuickFooter.vue'
-import LandingCollage from './LandingCollage.vue'
-import LandingLoginPane from './LandingLoginPane.vue'
+  import QuickFooter from '@quick/components/QuickFooter.vue'
+  import LandingCollage from './LandingCollage.vue'
+  import LandingLoginPane from './LandingLoginPane.vue'
+  import CakeSvg from '@quick/assets/cake.svg'
+  import CameraSvg from '@quick/assets/camera.svg'
+  import CoupleSvg from '@quick/assets/couple.svg'
+  import InlineSvg from 'vue-inline-svg'
+
+  const menus = [
+    { path: '/#', label: 'home' },
+    { path: '/#', label: 'fotografer' },
+    { path: '/#', label: 'lokasi' },
+    { path: '/#', label: 'tentang kami' },
+    { path: '/#', label: 'tentang kami' },
+    { path: '/#', label: 'produk' }
+  ]
 </script>
 
+<style scoped>
+  #hero {
+    background: linear-gradient(to bottom, rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url(/landing.jpg);
+    background-size: cover;
+    background-position: 0% 100%;
+    height: 100vh;
+  }
+
+  #hero > nav {
+    /*background: rgba(0, 0, 0, 0.1);*/
+  }
+</style>
 
 <template>
-  <div class="h-20 border-b border-gray-200 flex flex-col justify-center items-center">
-    <h1 class="font-bold text-gray-800 text-xl">Quick Photography</h1>
-    <h2 class="text-gray-600 text-sm">Aplikasi Penentuan Spot Prewedding</h2>
-  </div>
-  <LandingLoginPane/>
-  <LandingCollage/>
-  <QuickFooter/>
+  <section id="hero" class="w-screen flex flex-col text-white">
+    <nav class="w-4/5 h-20 flex items-center mx-auto">
+      <h1 class="flex-grow font-black text-2xl">quick photography</h1>
+      <div class="w-3/5 flex items-center justify-between">
+        <button class="hover:text-pink-400" v-for="menu in menus" :key="menu.label">
+          {{ menu.label }}
+        </button>
+      </div>
+    </nav>  
+    <div class="flex-grow flex flex-col items-center justify-center">
+      <h2 class="text-4xl font-thin">Applikasi SPK Penentuan Lokasi Prewedding</h2>
+      <h1 class="font-mono text-6xl font-black">Quick Photography</h1>
+      <button class="btn">tentukan lokasi anda</button>
+    </div>
+  </section>
+  <section class="w-screen h-64">
+  </section>
+  <!-- <div class="gradient bg-gradient-to-r from-purple-700 via-pink-600 to-red-600 text-white flex flex-col">
+    <div class="h-24 container mx-auto flex items-center">
+      <div class="flex items-center">
+        <router-link to="/">
+           <InlineSvg
+            :src="CameraSvg"
+            class="w-8 h-8 mr-2"
+          />
+        </router-link>
+        <h1 class="font-black text-lg lowercase">Quick Photography</h1>
+      </div>
+      <div class="flex-grow"></div>
+      <div>
+      </div>
+    </div>
+
+    <div class="flex-grow container mx-auto flex items-center">
+      <div class="md:w-1/2 text-white">
+        <h1 class="text-6xl font-bold mb-2 lowercase">Quick Photography</h1>
+        <h2 class="text-3xl mb-2">SPK Penentuan Lokasi Foto Prewedding</h2>
+        <h3 class="text-sm">If you want to create fantastic and unique visual experiences on the web, you will eventually need two elements to overlap or exist in the same place. Free Download Wedding Cake 29 SVG vector file in monocolor and multicolor type for Sketch or Illustrator from Wedding Cake 29 Vectors svg vector collection.</h3>
+      </div>
+      <div class="md:w-1/2 flex items-center justify-center">
+        <InlineSvg
+          :src="CakeSvg"
+          class="w-64 h-64 mr-2"
+        />
+      </div>
+    </div>
+
+    <QuickFooter/>
+  </div> -->
 </template>
